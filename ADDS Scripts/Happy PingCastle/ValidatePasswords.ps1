1d5be7b2e328348a1498b7884e49f2910349af3e
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2025-02-03 11:22:28 +0100

    Update from local

--- ADDS Scripts/Happy PingCastle/ValidatePasswords.ps1
+++ ADDS Scripts/Happy PingCastle/ValidatePasswords.ps1
@@ -4,26 +4,43 @@
     - The purpose is to ensure that every account has a password which is compliant with password expiration policies
 
 
-    Check for short password length in password policy
-    - The purpose is to verify if the password policy of the domain enforces users to have at least 8 characters in their password
-
-
     Check if all admin passwords are changed on the field
     - The purpose is to ensure that all admins are changing their passwords at least every 3 years 
 
 
+    Check for short password length in password policy
+    - The purpose is to verify if the password policy of the domain enforces users to have at least 8 characters in their password
+
 #>
+
+$DomainAdmins = Get-ADGroupMember "Domain Admins"
 
 
 # Validate that Domain Admins have updated the password.
 # --------------------------------------------------
 $PasswordAge = 180
-$OldAdmPwd = Get-ADGroupMember "Domain Admins" | Get-ADUser -Properties PasswordLastSet | Where-Object {
+$DomainAdmins.DistinguishedName | Get-ADUser -Properties PasswordLastSet | Where-Object {
     $_.PasswordLastSet -lt (Get-Date).adddays(-$PasswordAge) -and $_.Enabled -eq $True
-} | Select-Object -Property Name, PasswordLastSet, DistinguishedName | Out-GridView -Title "Please update password on listed users ASAP" -OutputMode Multiple
+} | Select-Object -Property Name, PasswordLastSet, DistinguishedName | Out-GridView -Title "Please update password on listed users ASAP" -OutputMode None
 
-if ($null -ne $OldAdmPwd) {
 
-#    Disable-ADAccount -Identity $OldAdmPwd.DistinguishedName
 
+# Make sure members of Domain Admin must change password
+# --------------------------------------------------
+$SelectedUsers = $DomainAdmins.DistinguishedName | Get-ADUser -Properties PasswordNeverExpires | Where-Object {
+    $_.PasswordNeverExpires -and $_.objectClass -eq "user" -and $_.Name -ne "Administrator"
+} | Select-Object Name, UserPrincipalName, DistinguishedName | Out-GridView -Title "Select the admins that are NOT used as service accounts" -OutputMode Multiple
+
+
+if ($Null -ne $SelectedUsers) {
+
+    $DomainAdmins.DistinguishedName | Set-ADUser -PasswordNeverExpires $false
+
+}
+
+
+# Update Default Domain Password Policy
+$PasswordLength = 14
+if ((Get-ADDefaultDomainPasswordPolicy -Identity $(Get-ADDomain).DNSRoot).MinPasswordLength -lt $PasswordLength) {
+    Set-ADDefaultDomainPasswordPolicy -MinPasswordLength $PasswordLength -Identity $DomainInfo.DNSRoot
 }
