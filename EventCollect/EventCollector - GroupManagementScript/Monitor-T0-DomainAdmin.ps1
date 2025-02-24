89499aec206e243c3d9cd79ca46d22327c8eb86d
Jan Kristensen <96570455+SysAdminDk@users.noreply.github.com>
2025-02-24 08:26:12 +0100

    Add Scripts

---
+++ EventCollect/EventCollector - GroupManagementScript/Monitor-T0-DomainAdmin.ps1
@@ -0,0 +1,208 @@
+﻿param
+(
+    $TargetSid,
+    $MemberSid,
+    $ActionBy,
+    $ActionByDomainName
+)
+
+
+# --
+# Dateformats (For logging)
+# --
+$Date = (Get-Date -Format "dd-MM-yyyy hh:mm:ss")
+$ScriptStart = [DateTimeOffset]::Now.ToUnixTimeMilliseconds()
+$EventData = @()
+
+
+# --
+# We need the AD powershell module
+# --
+Import-Module ActiveDirectory -Force
+<#
+ $TargetSid = "S-1-5-21-2296055821-4046125481-3868048633-512"
+ $TargetSid = "S-1-5-21-2296055821-4046125481-3868048633-519"
+ $TargetSid = "S-1-5-21-2296055821-4046125481-3868048633-1116"
+
+ $MemberSid = "S-1-5-21-2296055821-4046125481-3868048633-1604"
+ $MemberSid = "S-1-5-21-2296055821-4046125481-3868048633-1603"
+ $MemberSid = "S-1-5-21-2296055821-4046125481-3868048633-1602"
+#>
+
+# --
+# Lookup group from SID
+# --
+$ADGroup = Get-ADGroup -Identity $TargetSid
+
+# --
+# Lookup member from SID
+# --
+$GroupMember = Get-ADUser -Identity $MemberSid
+
+
+# --
+# Logfile location
+# --
+if ($PSScriptRoot -eq $Null) {
+    $LogFile = $PSScriptRoot + "\Logs\" + $ScriptStart + "_GroupMonitor.csv"
+} else {
+    $LogFile = "C:\TS-Data\Logs\" + $ScriptStart + "_GroupMonitor.csv"
+}
+# If more that one sessions is started witin same Millisecond, change filename
+#$FileTestCount = ((Get-ChildItem -Path (Split-Path $LogFile -Parent) -Filter $ScriptStart*).FullName).Count
+#If ($FileTestCount -ne 0) {
+#    $Logfile = $Logfile -replace(".txt"," ($($FileTestCount+1)).txt")
+#}
+# Write CSV header to file
+Add-Content -Path $LogFile -Value "Date;Domain\User;AD Group;Group Member;Action"
+
+
+# --
+# Only do stuff on selected Groups
+# --
+switch (($TargetSid -split("-"))[-1]) {
+    # --
+    # Domain Admins = S-1-5-21-2296055821-4046125481-3868048633-512
+    # --
+    512 {
+        # --
+        # Verify All other members have TTL
+        # Skip Administrator, BreakGlassAdmin and SVC-Syslog
+        # -
+        $AllowedMembers = "Administrator|BreakGlassAdmin"
+        $GroupMembers = (Get-ADGroup $TargetSid -Property member -ShowMemberTimeToLive).Member | Where {$_.Member -NotMatch $AllowedMembers}
+        foreach ($Member in $GroupMembers) {
+            # If there is no TTL, remove group member
+            if ($Member -NotMatch '^<TTL\=[0-9]+>') {
+                Remove-ADGroupMember -Identity "Domain Admins" -Members $($Member -replace("^<TTL\=[0-9]+>\,")) -Confirm:$false
+                Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);RemoveMember"
+            }
+        }
+
+        Break
+    }
+
+    # --
+    # Enterprise Admins = S-1-5-21-2296055821-4046125481-3868048633-519
+    # --
+    519 {
+        # --
+        # Verify All members have TTL
+        # Skip Administrator
+        # -
+        $AllowedMembers = "Administrator"
+        $GroupMembers = (Get-ADGroup $TargetSid -Property member -ShowMemberTimeToLive).Member | Where {$_.Member -NotMatch $AllowedMembers}
+        foreach ($Member in $GroupMembers) {
+            # If there is no TTL, remove group member
+            if ($Member -NotMatch '^<TTL\=[0-9]+>') {
+                Remove-ADGroupMember -Identity "Enterprise Admins" -Members "$($Member -replace("^<TTL\=[0-9]+>\,"))" -Confirm:$false
+                Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);RemoveMember"
+            }
+        }
+
+        Break
+    }
+
+    # --
+    # T0 - Domain Admin = S-1-5-21-2296055821-4046125481-3868048633-1110
+    # --
+    1110 {
+
+        # --
+        # Default TTL for Domain Admins
+        # --
+        $ttl = New-TimeSpan -Minutes 5
+
+        # --
+        # If New Member matches selected user type
+        # Add user to Domain Admins with TTL
+        # --
+        if ($GroupMember.DistinguishedName -Like "*OU=Tier 0*") {
+            Add-ADGroupMember -Identity "Domain Admins" -Members $GroupMember.DistinguishedName -MemberTimeToLive $ttl
+            Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);RemoveMember"
+        }
+
+        # --
+        # Remove user from T0 - Domain Admin (Reset)
+        # --
+        Remove-ADGroupMember -Identity "T0 - Domain Admin" -Members $GroupMember.DistinguishedName -Confirm:$false
+        Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;T0 - Domain Admin;$($GroupMember.Name);Cleanup"
+
+        Break
+    }
+
+    # --
+    # T0 - Enterprise Admin = S-1-5-21-2296055821-4046125481-3868048633-1113
+    # --
+    1113 {
+
+        # --
+        # Default TTL for Enterprise Admins
+        # --
+        $ttl = New-TimeSpan -Minutes 5
+
+        #$NewGroupMember = Get-Aduser -Identity $($GroupMember.DistinguishedName)
+        # --
+        # If New Member matches selected user type
+        # Add user to Domain Admins with TTL
+        # --
+        if ($GroupMember.DistinguishedName -Like "*OU=Tier 0*") {
+            Add-ADGroupMember -Identity "Domain Admins" -Members $GroupMember.DistinguishedName -MemberTimeToLive $ttl
+            Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);AddMember"
+        }
+
+        # --
+        # Remove user from T0 - Domain Admin (Reset)
+        # --
+        Remove-ADGroupMember -Identity "T0 - Domain Admin" -Members $GroupMember.DistinguishedName -Confirm:$false
+        Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);RemoveMember"
+
+        Break
+    }
+
+    # --
+    # T0 - Group Management = S-1-5-21-2296055821-4046125481-3868048633-1116
+    # --
+    1116 {
+        # --
+        # Ensure only member is "SVC-Syslog"
+        # -
+        $GroupMembers = (Get-ADGroup $TargetSid -Property member -ShowMemberTimeToLive).Member | Where {$_ -notlike "CN=SVC-Syslog*"}
+        foreach ($Member in $GroupMembers) {
+            Remove-ADGroupMember -Identity $TargetSid -Members $Member -Confirm:$false
+            Add-Content -Path $LogFile -Value "$Date;$ActionByDomainName\$ActionBy;$($ADGroup.Name);$($GroupMember.Name);RemoveMember"
+        }
+
+        Break
+    }
+
+    # --
+    # If not one og the groups we need to manage, just quit.
+    # --
+    Default {
+        exit
+    } 
+}
+
+#$EventData | Out-File -FilePath $LogFile -Encoding utf8 -Append
+$ScriptRunTime = $([DateTimeOffset]::Now.ToUnixTimeMilliseconds()) - $ScriptStart
+foreach ($data in $eventdata) {
+    Add-Content -Path $LogFile -Value "Runtime:$ScriptRunTime"
+}
+
+<#
+Get-Variable TargetSid | Remove-Variable
+Get-Variable MemberSid | Remove-Variable
+Get-Variable ActionBy | Remove-Variable
+Get-Variable ActionByDomainName | Remove-Variable
+Get-Variable Date | Remove-Variable
+Get-Variable ScriptStart | Remove-Variable
+Get-Variable EventData | Remove-Variable
+Get-Variable ADGroup | Remove-Variable
+Get-Variable GroupMember | Remove-Variable
+Get-Variable LogFile | Remove-Variable
+Get-Variable GroupMembers | Remove-Variable
+Get-Variable Member | Remove-Variable
+Get-Variable ttl | Remove-Variable
+Get-Variable ScriptRunTime | Remove-Variable
+#>
---
+++ EventCollect/Setup-EventCollector.ps1
@@ -0,0 +1,140 @@
+﻿<#
+     _____                _    ______                               _ _             
+    |  ___|              | |   |  ___|                             | (_)            
+    | |____   _____ _ __ | |_  | |_ ___  _ ____      ____ _ _ __ __| |_ _ __   __ _ 
+    |  __\ \ / / _ \ '_ \| __| |  _/ _ \| '__\ \ /\ / / _` | '__/ _` | | '_ \ / _` |
+    | |___\ V /  __/ | | | |_  | || (_) | |   \ V  V / (_| | | | (_| | | | | | (_| |
+    \____/ \_/ \___|_| |_|\__| \_| \___/|_|    \_/\_/ \__,_|_|  \__,_|_|_| |_|\__, |
+                                                                               __/ |
+                                                                              |___/ 
+    Todo.
+    1. Add the Collector server
+    2. ReTest LogForwarder setup.
+
+
+    "Script" Actions.
+    1. Setup Event forwarding
+    2. Add GPO
+    3. Add EventID to log Collection
+
+#>
+
+# --
+# 
+# --
+#Add-KDSRootKey -EffectiveImmediately
+
+
+# --
+# Set Event Collector server name
+# --
+$EventCollector = Get-ADComputer -Identity "RPA-01"
+
+
+# Create eventcollector GPO
+# ------------------------------------------------------------
+try {
+    $GPO = Get-GPO -Name "Admin - Domain Controllers Event Collection Policy"
+}
+Catch {
+    $GPO = New-GPO -Name "Admin - Domain Controllers Event Collection Policy"
+}
+(Get-GPO -Name $GPO.DisplayName).GpoStatus = "UserSettingsDisabled"
+
+
+# Update PDC security descriptor from PDC, to allow Network Services access.
+# https://learn.microsoft.com/en-gb/troubleshoot/windows-server/group-policy/set-event-log-security-locally-or-via-group-policy
+# ------------------------------------------------------------
+$ChannelAccess = Invoke-Command -ComputerName $(Get-ADDomain).PDCEmulator -ScriptBlock {
+    $log = Get-WinEvent -ListLog "Security"
+    Return $log.SecurityDescriptor
+}
+if ($ChannelAccess -notlike "*(A;;0x1;;;NS)") {
+    $ChannelAccess = $ChannelAccess + "(A;;0x1;;;NS)"
+}
+
+
+# Define the Eventcollector Registry settings
+# ------------------------------------------------------------
+$SubscriptionManager = "Server=http://" + $Identity.DNSHostName + ":5985/wsman/SubscriptionManager/WEC,Refresh=600"
+
+
+# Create GPO settings.
+# ------------------------------------------------------------
+Set-GPRegistryValue -Name $GPO.DisplayName -Key "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager" -ValueName "SubscriptionManager" -Type String -Value $SubscriptionManager | Out-Null
+Set-GPRegistryValue -Name $GPO.DisplayName -Key "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\EventLog\Security" -ValueName "ChannelAccess" -Type String -Value $ChannelAccess | Out-Null
+
+
+# Link GPO to Domain controllers
+# ------------------------------------------------------------
+New-GPLink -Name $GPO.DisplayName -Target $((Get-ADDomain).DomainControllersContainer) -LinkEnabled Yes
+
+
+# Connect to "syslog" server
+# ------------------------------------------------------------
+$Session = New-PSSession -ComputerName $EventCollector.DNSHostName
+
+
+# Setup the event Collector server
+# ------------------------------------------------------------
+Invoke-Command -Session $Session {
+
+    # Configure Windows Event Collector service
+    # ------------------------------------------------------------
+    & wecutil qc /q | Out-Null
+
+
+    # Ensure the Firewall Rules are updated.
+    # https://learn.microsoft.com/en-us/troubleshoot/windows-server/admin-development/events-not-forwarded-by-windows-server-collector
+    # ------------------------------------------------------------
+    & netsh http delete urlacl url=http://+:5985/wsman/ | Out-Null
+    & netsh http add urlacl url=http://+:5985/wsman/ sddl="D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)" | Out-Null
+    & netsh http delete urlacl url=https://+:5986/wsman/ | Out-Null
+    & netsh http add urlacl url=https://+:5986/wsman/ sddl="D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)" | Out-Null
+
+
+    # Create Event Subscription, add more IDs if needed.
+    # ------------------------------------------------------------
+    # EventID=4728 = A member was added to a security-enabled global group
+    # EventID=4729 = A member was removed from a security-enabled global group
+    # EventID=5136 = A directory service object was modified
+    # ------------------------------------------------------------
+    $CollectorName = "Group membership Changes"
+    $CollectorPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\EventCollector\Subscriptions"
+    New-Item -Path $CollectorPath -Name $CollectorName | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name Enabled -PropertyType dword -Value 0 | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name Description -PropertyType string -Value "" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name URI -PropertyType string -Value "http://schemas.microsoft.com/wbem/wsman/1/windows/EventLog" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name ConfigurationMode -PropertyType string -Value "MinLatency" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name Query -PropertyType string -Value "<QueryList><Query Id=`"0`"><Select Path=`"Security`">*[System[(EventID=4728 or EventID=4729 or EventID=5136)]]</Select></Query></QueryList>" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name TransportName -PropertyType string -Value "HTTP" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name LogFile -PropertyType string -Value "ForwardedEvents" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name PublisherName -PropertyType string -Value "Microsoft-Windows-EventCollector" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name ReadExistingEvents -PropertyType dword -Value 0 | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name SubscriptionType -PropertyType string -Value "SourceInitiated" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name AllowedIssuerCAs -PropertyType string -Value "" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name AllowedSourceDomainComputers -PropertyType string -Value "O:NSG:BAD:P(A;;GA;;;DD)S:" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name LastError -PropertyType dword -Value 0 | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name LastFaultMessage -PropertyType string -Value "" | Out-Null
+    New-ItemProperty -Path "$CollectorPath\$CollectorName" -Name LastErrorTime -PropertyType string -Value "" | Out-Null
+
+    New-Item -Path "$CollectorPath\$CollectorName" -Name "EventSources" | Out-Null
+
+    Set-ItemProperty -Path"$CollectorPath\$CollectorName" -Name Enabled -Value 1 | Out-Null
+
+    & wecutil rs "$CollectorName"
+
+}
+
+
+# Update GPOs and restart domain controllers
+# ------------------------------------------------------------
+$((Get-ADDomain).ReplicaDirectoryServers) | % { Invoke-Command -ComputerName $_ -ScriptBlock {
+        Invoke-GPUpdate -Force | Out-Null
+        Invoke-GPUpdate -Force | Out-Null
+   
+        Shutdown -r -t 10
+    }
+
+    Start-Sleep -Seconds 300
+}
